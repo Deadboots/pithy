2a2282c6dfec789ab230650e09958e5dfa4bb3d4
Joshua Kozemzak <jkozemzak@gmail.com>
2014-12-15 16:24:43 -0700

    First REAL commit

---
+++ input.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.4
+
+name = input("Enter your name:")
+
+age = input("Enter your age:")
+
+sex = input("Enter your sex (male or female):")
+
+lang = input("Enter your native language:")
+
+print(name.capitalize())
+print(age)
+print(sex.upper())
+print(lang.upper())
